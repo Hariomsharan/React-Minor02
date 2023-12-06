cb3dbfa5c60aaaaa4ee727e480b09e70e08acdc6
Hariom Sharan <hariomsharan2771@gmail.com>
2023-12-06 20:42:42 +0530

    update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3050/'
+    baseURL: 'https://minor02backend.onrender.com/'
 });
 
 export default instance;
